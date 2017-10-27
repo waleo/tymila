167ab1912091ab695faee7d22ef38bdfd9c683ae
waleo <info@railsfever.com>
2017-10-27 19:54:04 -0400

    automate js minification

---
+++ minify-js.sh
@@ -0,0 +1,2 @@
+cd assets/js
+uglifyjs ./plugins/jquery1.11.0.min.js plugins/bootstrap.min.js plugins/jquery.easing.1.3.min.js plugins/modernizr.custom.min.js plugins/plugins.min.js custom.js -o all.min.js
